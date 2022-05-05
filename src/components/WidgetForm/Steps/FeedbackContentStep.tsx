963fabb3322a92b491494372e8647401eb969697
FinotelliCarlos <carlos.finotelli@outlook.com>
2022-05-04 22:57:56 -0300

    create steps

---
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,31 @@
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from ".."
+import { CloseButton } from "../../CloseButton"
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested : () => void;
+}
+
+export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: FeedbackContentStepProps){
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+  return(
+    <>
+    <header className="">
+      <button onClick={onFeedbackRestartRequested} type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+        <ArrowLeft weight="bold" className="w-4 h-4" />
+      </button>
+
+      <span className="text-xl leading-6 flex items-center gap-2">
+        <img className="w-6 h-6" src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} />
+        {feedbackTypeInfo.title}
+      </span>
+      <CloseButton />
+    </header>
+    <div className="flex pt-8 gap-2 w-full">
+
+    </div>
+  </>
+  )
+}
