551fbe5dcf529e76e8072a7a3818f3fd3ead47d7
FinotelliCarlos <carlos.finotelli@outlook.com>
2022-05-05 12:21:07 -0300

    getFormData

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -15,6 +15,14 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: 
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
+  function handleSubmitFeedback(event:FormEvent){
+    event.preventDefault()
+
+    console.log({
+      screenshot,comment
+    })
+  }
+
   return(
     <>
     <header className="">
@@ -29,7 +37,7 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: 
       <CloseButton />
     </header>
 
-    <form className="my-4 w-full">
+    <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
       <textarea 
         className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
         placeholder="Conte com detalhes oque está acontecendo..."
